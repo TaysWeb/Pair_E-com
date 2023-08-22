7d1e97fe3175c8c1ad66c3ee02cec0b762fe5359
laeeq <laeeq.majal@gmail.com>
2023-08-22 15:58:53 +0200

    qwerty

--- frontEnd/src/store/index.js
+++ frontEnd/src/store/index.js
@@ -1,14 +1,122 @@
-import { createStore } from 'vuex'
-
+import { createStore } from "vuex";
+import axios from "axios";
+const miniURL = "https://sixth-zp4e.onrender.com/products";
 export default createStore({
   state: {
+    users: null,
+    user: null,
+    products: null,
+    product: null,
+    spinner: null,
+    token: null,
+    msg: null,
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    setUsers(state, users) {
+      state.users = users;
+    },
+    setUser(state, user) {
+      state.user = user;
+    },
+    setProducts(state, products) {
+      state.products = products;
+      console.log(products)
+    },
+    setProduct(state, product) {
+      state.product = product;
+    },
+    setSpinner(state, spinner) {
+      state.spinner = value;
+    },
+    setToken(state, token) {
+      state.token = token;
+    },
+    setMsg(state, msg) {
+      state.msg = msg;
+    },
   },
   actions: {
+    async fetchUsers(context) {
+      try {
+        const { data } = await axios.get(`${miniURL}users`);
+        context.commit("setUsers", data.results);
+      } catch (e) {
+        context.commit("setMsg", "an error occured");
+      }
+    },
+    async fetchUser(context) {
+      try {
+        const { data } = await axios.get(`${miniURL}user`);
+        context.commit("setUser", data.results);
+      } catch (e) {
+        context.commit("setMsg", "an error occured");
+      }
+    },
+    async fetchProducts(context) {
+      try {
+        const { data } = await axios.get(`${miniURL}`);
+        context.commit("setProducts", data.results);
+      } catch (e) {
+        context.commit("setMsg", "an error occured");
+      }
+    },
+    async fetchProduct(context) {
+      try {
+        const { data } = await axios.get(`${miniURL}`);
+        context.commit("setProduct", data.results);
+      } catch (e) {
+        context.commit("setMsg", "an error occured");
+      }
+    },
+    async createUser(context) {
+      try{
+        const { data } = await axios.post(`${miniURL}user`)
+        context.commit("setUser", data.results);
+      } catch (e) {
+        context.commit("setMsg", "an error occured");
+      }
+    },
+    async updateUser(context) {
+      try{
+        const { data } = await axios.patch(`${miniURL}user`)
+        context.commit("setUser", data.results);
+      } catch (e) {
+        context.commit("setMsg", "an error occured");
+      }
+    },
+    async deleteUser(context) {
+      try{
+        const { data } = await axios.delete(`${miniURL}user`)
+        context.commit("setUser", data.results);
+      } catch (e) {
+        context.commit("setMsg", "an error occured");
+      }
+    },
+    async createProduct(context) {
+      try{
+        const { data } = await axios.post(`${miniURL}product`)
+        context.commit("setProduct", data.results);
+      } catch (e) {
+        context.commit("setMsg", "an error occured")
+      }
+    },
+    async updateProduct(context) {
+      try{
+        const { data } = await axios.patch(`${miniURL}product`)
+        context.commit("setProduct", data.results)
+      } catch (e) {
+        context.commit("setMsg", "an error occured")
+      }
+    },
+    async deleteProduct(context) {
+      try{
+        const { data } = await axios.delete(`${miniURL}product`)
+        context.commit("setProduct", data.results);
+      } catch (e) {
+        context.commit("setMsg", "an error occured")
+      }
+    }
   },
-  modules: {
-  }
-})
+  modules: {},
+});
--- frontEnd/src/views/AdminView.vue
+++ frontEnd/src/views/AdminView.vue
@@ -1,14 +1,87 @@
 <template>
-
+    <div>
+<h1>Users Table</h1>
+<br>
+<table class="table">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Surname</th>
+        <th>Age</th>
+        <th>Email Address</th>
+        <th>Profile Image</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody v-for="user in users" :key="user">
+      <tr v-if="users">
+         <td>{{ user.firstName }}</td>
+         <td>{{ user.lastName }}</td>
+         <td>{{ user.userAge }}</td>
+         <td>{{ user.emailAdd }}</td>
+         <td><img :src="user.userProfile" :alt="user.firstName" loading="lazy" class="img-fluid image"></td>
+         <td><button class="edit">edit</button><button class="del">delete</button></td>
+      </tr>
+      <tr v-else>
+      </tr>
+    </tbody>
+  </table>
+<h1>Products Table</h1>
+<table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>name</th>
+        <th>category</th>
+        <th>product image</th>
+        <th>price</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody v-for="product in product" :key="product">
+      <tr v-if="product">
+        <td>{{ product.product_id}}</td>
+        <td>{{ product.product_name }}</td>
+        <td>{{ product.product_category }}</td>
+        <td><img :src="product.product_image" :alt="product.product_name" class="img-fluid image" loading="lazy"></td>
+        <td>R {{ product.product_price }}</td>
+        <td><button class="edit">edit</button><button class="del">delete</button></td>
+      </tr>
+      <tr v-else>
+      </tr>
+    </tbody>
+  </table>
+    </div>
 </template>
 <script>
-
-export default{
-components: {
-    Admin
-}
-}
+    export default {
+        computed: {
+        users() {
+            return this.$store.state.users
+        },
+        product() {
+            return this.$store.state.product
+        }
+    },
+    mounted() {
+        this.$store.dispatch('fetchProduct')
+        this.$store.dispatch('fetchUsers')
+    },
+    }
 </script>
 <style scoped>
-
+img{
+   width: 7rem;
+}
+.edit{
+    margin-right: 5px;
+    width: 3rem;
+    border-radius: 5px;
+    background-color: rgb(210, 205, 205);
+}
+.del{
+    width: 4rem;
+    border-radius: 5px;
+    background-color: rgb(255, 129, 129);
+}
 </style>
